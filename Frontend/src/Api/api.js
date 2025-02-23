4fe5b4c6ead193b189b5efa9d6cc0be563c20246
nesi04 <neeraj579singh@gmail.com>
2025-02-23 22:15:06 +0530

    ff

--- Frontend/src/Api/api.js
+++ Frontend/src/Api/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const api = axios.create({baseURL:"http://localhost:4304/api"});
+const api = axios.create({baseURL:"https://portfolio-ohz3.onrender.com/api/"});
 
 export const getProjects = async ()=>{
     try {
